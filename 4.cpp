ae9632674a8f1fee9a18c164191caa1e4cbfbd72
Yashwanthk123 <106529220+Yashwanthk123@users.noreply.github.com>
2024-02-29 22:57:00 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float radius;
+    float height;
+    float volume;
+    cout<<"enter the radius of cylinder : ";
+    cin>>radius;
+    cout<<"enter the height of cylinder : ";
+    cin>>height;
+    cout<<"the volume of cylinder is : ";
+    cout<<3.14*radius*radius*height;
+
+
+}
---
+++ 5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char x;
+    char y;
+    cout<<"enter the first character : ";
+    cin>>x;
+    cout<<"enter the second character : ";
+    cin>>y;
+    cout<<int(x)-int(y);
+}
